e5a284bc442a8b19aa6d4a76326dc5b08db0103d
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-05-25 15:39:13 -0300

    Corrige req 5 e 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,3 @@
-SELECT * FROM northwind.purchase_orders
-WHERE notes LIKE "Purchase generated based on Order #3_";
+SELECT notes FROM northwind.purchase_orders
+WHERE notes LIKE 'Purchase%'
+AND notes LIKE '%3_';
